7de2d9881aab1c0113d6947377bf9e554010bb94
David Allsopp <david.allsopp@metastack.com>
2020-04-10 12:29:06 +0100

    At least note the expected hashes in example!
    Verifying them might be better...

--- examples/examples.ml
+++ examples/examples.ml
@@ -12,6 +12,7 @@ let () =
   begin match I.hash_raw ~t_cost ~m_cost ~parallelism ~pwd ~salt ~hash_len with
     | Result.Ok hash ->
       Printf.printf "argon2i hash:\n";
+      (* ec6891c09fc1461720e508485da42b2087ca9a708185d9dc890539a33cd2af6f *)
       I.hash_to_string hash
         |> String.iter (fun c -> Printf.printf "%02x" (Char.code c));
       Printf.printf "\n";
@@ -22,6 +23,7 @@ let () =
   begin match D.hash_raw ~t_cost ~m_cost ~parallelism ~pwd ~salt ~hash_len with
     | Result.Ok hash ->
       Printf.printf "argon2d hash:\n";
+      (* 30f5e9f2584e962bba213cc256dcf9ccdce18d8f67a6cbf8e012e0619b6d52d2 *)
       D.hash_to_string hash
         |> String.iter (fun c -> Printf.printf "%02x" (Char.code c));
       Printf.printf "\n";
