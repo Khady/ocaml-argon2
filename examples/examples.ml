850050aca72040a809d1949fe9740dee403aa359
David Allsopp <david.allsopp@metastack.com>
2020-04-10 12:25:02 +0100

    Fix examples/examples.ml

--- examples/examples.ml
+++ examples/examples.ml
@@ -12,25 +12,26 @@ let () =
   begin match I.hash_raw t_cost m_cost parallelism password salt hash_len with
     | Result.Ok hash ->
       Printf.printf "argon2i hash:\n";
-      String.iter (fun c -> Printf.printf "%02x" (Char.code c)) hash;
+      I.hash_to_string hash
+        |> String.iter (fun c -> Printf.printf "%02x" (Char.code c));
       Printf.printf "\n";
     | Result.Error e ->
       Printf.printf "Error while computing hash with argon2i_hash_raw: %s\n"
-        (Argon2_ErrorCodes.message e)
+        (ErrorCodes.message e)
   end;
   begin match D.hash_raw t_cost m_cost parallelism password salt hash_len with
     | Result.Ok hash ->
       Printf.printf "argon2d hash:\n";
-      String.iter (fun c -> Printf.printf "%02x" (Char.code c)) hash;
+      D.hash_to_string hash
+        |> String.iter (fun c -> Printf.printf "%02x" (Char.code c));
       Printf.printf "\n";
     | Result.Error e ->
       Printf.printf "Error while computing hash with argon2d_hash_raw: %s\n"
-        (Argon2_ErrorCodes.message e)
+        (ErrorCodes.message e)
   end;
   begin
     match hash t_cost m_cost parallelism password salt
-            Argon2_type.Argon2_d hash_len encoded_len
-            Argon2_version.ARGON2_VERSION_NUMBER
+            D hash_len encoded_len VERSION_NUMBER
     with
     | Result.Ok (hash, encoded) ->
       Printf.printf "hash argon2d:\n";
@@ -40,13 +41,13 @@ let () =
       String.iter (fun c -> Printf.printf "%02x" (Char.code c)) encoded;
       Printf.printf "\n";
       begin
-        match verify encoded "password" Argon2_type.Argon2_d with
+        match verify encoded "password" D with
         | Result.Ok _ -> Printf.printf "verify OK\n"
         | Result.Error e ->
           Printf.printf "Error while computing verify: %s\n"
-            (Argon2_ErrorCodes.message e)
+            (ErrorCodes.message e)
       end;
     | Result.Error e ->
       Printf.printf "Error while computing hash: %s\n"
-        (Argon2_ErrorCodes.message e)
+        (ErrorCodes.message e)
   end;
