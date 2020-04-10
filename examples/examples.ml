fd26de01f15b5aa2533fb515dba846f3c7901f15
David Allsopp <david.allsopp@metastack.com>
2020-04-10 12:29:37 +0100

    Refactor examples/examples.ml printing function

--- examples/examples.ml
+++ examples/examples.ml
@@ -1,6 +1,7 @@
 open Argon2
 
 let () =
+  let message fmt = Printf.ksprintf (Printf.printf "%s\n%!") fmt in
   let hash_len = 32 in
   let t_cost = 2 in
   let m_cost = 65536 in
@@ -11,24 +12,24 @@ let () =
   let encoded_len = encoded_len ~t_cost ~m_cost ~parallelism ~salt_len ~hash_len in
   begin match I.hash_raw ~t_cost ~m_cost ~parallelism ~pwd ~salt ~hash_len with
     | Result.Ok hash ->
-      Printf.printf "argon2i hash:\n";
+      message "argon2i hash:";
       (* ec6891c09fc1461720e508485da42b2087ca9a708185d9dc890539a33cd2af6f *)
       I.hash_to_string hash
         |> String.iter (fun c -> Printf.printf "%02x" (Char.code c));
-      Printf.printf "\n";
+      message "";
     | Result.Error e ->
-      Printf.printf "Error while computing hash with argon2i_hash_raw: %s\n"
+      message "Error while computing hash with argon2i_hash_raw: %s"
         (ErrorCodes.message e)
   end;
   begin match D.hash_raw ~t_cost ~m_cost ~parallelism ~pwd ~salt ~hash_len with
     | Result.Ok hash ->
-      Printf.printf "argon2d hash:\n";
+      message "argon2d hash:";
       (* 30f5e9f2584e962bba213cc256dcf9ccdce18d8f67a6cbf8e012e0619b6d52d2 *)
       D.hash_to_string hash
         |> String.iter (fun c -> Printf.printf "%02x" (Char.code c));
-      Printf.printf "\n";
+      message "";
     | Result.Error e ->
-      Printf.printf "Error while computing hash with argon2d_hash_raw: %s\n"
+      message "Error while computing hash with argon2d_hash_raw: %s"
         (ErrorCodes.message e)
   end;
   begin
@@ -37,20 +38,19 @@ let () =
             ~version:VERSION_NUMBER
     with
     | Result.Ok (hash, encoded) ->
-      Printf.printf "hash argon2d:\n";
+      message "hash argon2d:";
       String.iter (fun c -> Printf.printf "%02x" (Char.code c)) hash;
-      Printf.printf "\n";
-      Printf.printf "encoded argon2d:\n";
+      message "\nencoded argon2d:";
       String.iter (fun c -> Printf.printf "%02x" (Char.code c)) encoded;
-      Printf.printf "\n";
+      message "";
       begin
         match verify ~encoded ~pwd ~kind:D with
-        | Result.Ok _ -> Printf.printf "verify OK\n"
+        | Result.Ok _ -> message "verify OK"
         | Result.Error e ->
-          Printf.printf "Error while computing verify: %s\n"
+          message "Error while computing verify: %s"
             (ErrorCodes.message e)
       end;
     | Result.Error e ->
-      Printf.printf "Error while computing hash: %s\n"
+      message "Error while computing hash: %s"
         (ErrorCodes.message e)
   end;
